b8bedada68ca6fe0e524e5092bb72f7e46254c8e
Eric Schneider <franz.eric.schneider@gmail.com>
2015-10-01 23:19:58 -0400

    Preliminary push

--- python/dynamics_parse.py
+++ python/dynamics_parse.py
@@ -43,17 +43,43 @@ def main():
     # Correct for offsets
     # offset_from_origin = []
 
+    # Find peaks
+    peak_indices = find_peaks(mass_z)
+    print("peak_indices: ")
+    print(peak_indices)
+
     # Taken from here: http://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html#mplot3d-tutorial
     # and this example: http://matplotlib.org/mpl_examples/mplot3d/lines3d_demo.py
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.plot([base_x], [base_y], [base_z], label='base')
     ax.plot(mass_x, mass_y, mass_z, color='g', label='mass path')
+    # ax.plot()
     ax.set_xlim3d(-0.5, 0.5)
     ax.set_ylim3d(-0.5, 0.5)
     ax.set_zlim3d(-0.75, 0.25)
     ax.legend()
     plt.show()
+
+
+def find_peaks(Z):
+    '''
+    Returns
+      - num_peaks
+      - peak_indices
+    '''
+    peak_indices = []
+    check_range = 25  # 25 points on either side of the peak
+    for i in range(len(Z)):
+        if i <= check_range or i >= len(Z) - check_range:
+            pass
+        elif sum(Z[i - check_range : i] < Z[i]) == 0 and sum(Z[i] < Z[i + 1 : i + check_range]) == 0:
+            print("Found a local maximum at i = %d", i)
+            peak_indices.append(i)
+        elif sum(Z[i - check_range : i] > Z[i]) == 0 and sum(Z[i] > Z[i + 1 : i + check_range]) == 0:
+            print("Found a local minimum at i = %d", i)
+            peak_indices.append(i)
+    return peak_indices
 
 
 if __name__ == '__main__':
